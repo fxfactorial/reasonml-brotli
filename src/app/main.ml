b219a78e29250391eefaba1f35b7666d1aa630ea
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-09-26 00:13:58 -0700

    Change name to brozip, build out cmd line usage

--- src/app/main.ml
+++ src/app/main.ml
@@ -1,8 +1,34 @@
-(* I probably need to use big array *)
+open Cmdliner
+open Lwt
+
+let program items =
+  List.iter print_endline items;
+  `Ok ()
+
+let compressed =
+  let doc = "Source file(s) to copy." in
+  Arg.(value & (pos_all file) [] & info [] ~docv:"FILE or DIR" ~doc)
+
+let cmd =
+  let doc = "brozip is a tool to concurrently compress/decompress files \
+             using the Brotli compression algorithm"
+  in
+  let man = [`S "AUTHOR";
+             `P "brozip was written by Edgar Aroutiounian";
+             `S "BUGS";
+             `P "See development at http://github.com/fxfactorial/ocaml-brotli";
+             `S "MISC";
+             `P "$(tname) is written in OCaml with bindings to Google's \
+                 Brotli C/C++ library"]
+  in
+  Term.(pure program $ compressed),
+  Term.info "brozip" ~version:"0.1" ~doc ~man
+
+let prog =
+  match Term.eval cmd with
+  | `Ok _ -> ()
+  | `Error _ -> prerr_endline "something went wrong"
+  | _ -> ()
+
 let () =
-  (* open_in_bin Sys.argv.(1) |> IO.input_channel |> IO.read_all *)
-  (* |> Brotli.decompress_buffer *)
-  (* |> print_endline *)
-  Sys.argv.(1)
-  |> Brotli.decompress_buffer
-  |> print_endline
+  prog |> return |> Lwt_main.run
