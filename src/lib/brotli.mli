0063992405fc2e4fab47168a3b5326351b232eba
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-09-26 20:59:34 -0700

    Add documentation as oasis target

--- src/lib/brotli.mli
+++ src/lib/brotli.mli
@@ -3,10 +3,12 @@
 
 (** Decompresses a file and store at destination, if no destination
     given then store at given file origin name just without an
-    extension *)
+    extension of the original file name. i.e. foo.compressed becomes
+    foo *)
 val decompress_to_path : ?file_dest:string -> string -> unit Lwt.t
 
-(** Decompress a file at filepath and give back in memory *)
+(** Decompress a file at filepath and give back in memory the
+    decompressed contents as a Bigarray *)
 val decompress_to_mem :
   string ->
   (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t Lwt.t
