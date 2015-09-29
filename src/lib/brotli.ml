ac777617554d38a40e097208fbe83a30de788867
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-09-28 22:04:25 -0700

    Create parameter record, populate with variants to int values

--- src/lib/brotli.ml
+++ src/lib/brotli.ml
@@ -1,5 +1,25 @@
 open Lwt
 
+type mode =
+  | Generic
+  | Text
+  | Font
+
+type params = {mode : int;
+               quality : int;
+               lgwin : int;
+               lgblock : int; }
+
+type quality = [`_0 | `_1 | `_2 | `_3 | `_4 | `_5 |
+                `_6 | `_7 | `_8 | `_9 | `_10 | `_11]
+
+type lgwin = [`_10 | `_11 | `_12 | `_13 | `_14
+             | `_15 | `_16 | `_17 | `_18 | `_19
+             | `_20 | `_21 | `_22 | `_23 | `_24]
+
+type lgblock = [`_0 | `_16 | `_17 | `_18 | `_19
+               | `_20 | `_21 | `_22 | `_23 | `_24]
+
 external unpack_data_to_path :
   string ->
   ('char, 'int8_unsigned_elt, 'layout) Bigarray.Array1.t ->
@@ -72,19 +92,13 @@ end
 
 module Compress = struct
 
+  type mode =
+    | Generic
+    | Text
+    | Font
 
   type exn += Compression_failure of string
   type exn += Compression_param_invalid of string
-
-  type mode =
-    | Generic (** Compression is not aware of any special features of input *)
-    | Text    (** Compression knows that input is UTF-8 *)
-    | Font    (** Compression knows that input is WOFF 2.0 *)
-
-  type params = {mode : mode;
-                 quality : int;
-                 lgwin : int;
-                 lgblock : int; }
 
   type quality = [`_0 | `_1 | `_2 | `_3 | `_4 | `_5 |
                   `_6 | `_7 | `_8 | `_9 | `_10 | `_11]
@@ -95,6 +109,11 @@ module Compress = struct
 
   type lgblock = [`_0 | `_16 | `_17 | `_18 | `_19
                  | `_20 | `_21 | `_22 | `_23 | `_24]
+
+  let int_of_mode = function
+    | Generic -> 0
+    | Text -> 1
+    | Font -> 2
 
   let int_of_quality (x : quality) = match x with
     | `_0 -> 0 | `_1 -> 1 | `_2 -> 2 | `_3 -> 3 | `_4 -> 4 | `_5 -> 5
@@ -108,6 +127,12 @@ module Compress = struct
   let int_of_lgblock (x : lgblock) = match x with
     | `_0 | `_16 -> 16 | `_17 -> 17 | `_18 -> 18 | `_19 -> 19
     | `_20 -> 20 | `_21 -> 21 | `_22 -> 22 | `_23 -> 23 | `_24 -> 24
+
+  let make_params m q lgw lgb =
+    { mode = int_of_mode m;
+      quality = int_of_quality q;
+      lgwin = int_of_lgwin lgw;
+      lgblock = int_of_lgblock lgb; }
 
   let try_it = function
     | t -> try%lwt t with | Failure s -> raise (Compression_failure s)
